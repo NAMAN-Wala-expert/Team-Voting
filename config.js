94c7cfe40eae1e252427b8a176d80f251e1af092
NAMAN-Wala-expert <107202669+NAMAN-Wala-expert@users.noreply.github.com>
2024-05-05 21:00:03 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,10 +1,16 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+        apiKey: "AIzaSyB_SlJGSvhBhWxcOf7vkBLAu3onRuCZt44",
+        authDomain: "project-67-c4a8a.firebaseapp.com",
+        projectId: "project-67-c4a8a",
+        storageBucket: "project-67-c4a8a.appspot.com",
+        messagingSenderId: "187376715605",
+        appId: "1:187376715605:web:3c9b1762998ac3aed3d1dc",
+        measurementId: "G-QDK6HFW7W7"
 };
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 export default firebase.database();
